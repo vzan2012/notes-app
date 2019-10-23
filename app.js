e433d9c747516b90b7bd029e48262e92f59755b8
Deepak Guptha <vzan2012@gmail.com>
2019-10-23 22:45:56 +0530

    feat(user): Challenge 3 updated

--- app.js
+++ app.js
@@ -2,10 +2,19 @@
 // const name = "Title Name";
 // console.log(add(5,-5));
 
+// Validator NPM Package 
 const validator = require('validator');
+
+// Chalk NPM Package 
+const chalk = require('chalk');
+
 const getNotes = require('./notes.js');
 
 getNotes();
 
 console.log(validator.isEmail('vzan2012@gmail.com'));
 console.log(validator.isURL('https://vzan2012.github.io'));
+
+const msg = 'Success';
+
+console.log(chalk.green.bold.inverse.underline(msg));
